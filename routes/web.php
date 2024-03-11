c7a8c221c9ac4206f67f9244db50ac6c31dad68d
ljacques <lionel.jacques@equasens.com>
2024-03-11 14:47:09 +0100

    plop

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::any('/{any}', [DicewayController::class, 'index'])->where('any', '^(?!api).*$');
