cff50f6490734be126a8e25a0d9909dbd32d70a7
ljacques <lionel.jacques@equasens.com>
2024-03-15 14:24:08 +0100

    login template

--- frontend/src/app/auth/public/login/login.component.ts
+++ frontend/src/app/auth/public/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CardModule} from 'primeng/card';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CardModule
+    CardModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
