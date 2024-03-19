b14ccf4ebe0c1312e8c35dfd0e9d5fd1640fd2c9
ljacques <lionel.jacques@equasens.com>
2024-03-19 15:09:43 +0100

    notice

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoggedInGuardService } from './auth/guards/logged-in-guard.service';
 import { ForgottenComponent } from './auth/public/forgotten/forgotten.component';
 import { LoginComponent } from './auth/public/login/login.component';
 import { RegisterComponent } from './auth/public/register/register.component';
+import { NoticeComponent } from './auth/public/notice/notice.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'forgotten', component: ForgottenComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'notice', component: NoticeComponent },
     { path: '', component: HomeComponent, canActivate: [LoggedInGuardService] },
     { path: '**', redirectTo: '/' },
 ];
