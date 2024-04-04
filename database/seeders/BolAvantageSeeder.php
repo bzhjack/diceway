1fe8e82bd39daa2e3e6cbc634066f6dff2a9dd44
ljacques <lionel.jacques@equasens.com>
2024-04-04 17:15:39 +0200

    login template

--- database/seeders/BolAvantageSeeder.php
+++ database/seeders/BolAvantageSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\BolAvantage;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BolAvantageSeeder extends Seeder
@@ -110,7 +109,7 @@ class BolAvantageSeeder extends Seeder
             [
                 'id'=> 17 ,
                 'avantage' => 'Cri de guerre',
-                'description' => 'Un dé malus pour les assailants pendant un round. 1 fois par jours ou une dépense de 1 point d’héroïsme'
+                'description' => 'Un dé malus pour les assailants pendant le round qui suit. 1 fois par jours ou une dépense de 1 point d’héroïsme'
             ],
             [
                 'id'=> 18 ,
@@ -140,7 +139,7 @@ class BolAvantageSeeder extends Seeder
                 'id'=> 22 ,
                 'avantage' => 'Fêtard',
                 'de_bonus' => true,
-                'de_bonus_domaine' => 'dans les tavernes pour recueillir des informations, des contacts ou des biens et des services. Resister aux effets de la boisson'
+                'de_bonus_domaine' => 'Dans les tavernes pour recueillir des informations, des contacts ou des biens et des services. Resister aux effets de la boisson'
             ],
             [
                 'id'=> 23 ,
@@ -153,6 +152,226 @@ class BolAvantageSeeder extends Seeder
                 'avantage' => 'Fortuné',
                 'de_bonus' => true,
                 'de_bonus_domaine' => 'Acquérir des biens ou des services dans votre cité d’origine.'
+            ],
+            [
+                'id'=> 25 ,
+                'avantage' => 'Gamin des rues',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Interagir avec les gens des bas-fonds, filer quelqu\'un ou remarquer un détail dans la rue.'
+            ],
+            [
+                'id'=> 26 ,
+                'avantage' => 'Inspirateur',
+                'description' => 'Un dé bonus pour les compagnons pendant le round qui suit. 1 fois par jours ou une dépense de 1 point d’héroïsme'
+            ],
+            [
+                'id'=> 27 ,
+                'avantage' => 'Intimidant',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Forcer quelqu’un à vous donner une information ou à faire une chose.'
+            ],
+            [
+                'id'=> 28 ,
+                'avantage' => 'Intrépide',
+                'description' => 'Insensible à la peur, même par la magie'
+            ],
+            [
+                'id'=> 29 ,
+                'avantage' => 'Laboratoire fourni',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Créer des préparations alchimiques ou des instruments mécaniques dans le laboratoire.'
+            ],
+            [
+                'id'=> 30 ,
+                'avantage' => 'Magie des Rois-Sorciers',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Lancer des sortilèges.',
+                'description' => 'vous devez prendre un désavantage supplémentaire.'
+            ],
+            [
+                'id'=> 31 ,
+                'avantage' => 'Mains guérisseuses',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Aider un blessé à récupérer de ses blessures, d’un empoisonnement, etc.',
+                'description' => 'Vous devez posséder la carrière médecin.'
+            ],
+            [
+                'id'=> 32 ,
+                'avantage' => 'Maître du déguisement',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Dissimuler votre identité.',
+                'description' => 'vous pouvez dépenser 1 point d’héroïsme pour faire partie du décor.'
+            ],
+            [
+                'id'=> 33 ,
+                'avantage' => 'Marqué par les dieux',
+                'attribut' => 'heroisme',
+                'attribut_bonus' => "1"
+            ],
+            [
+                'id'=> 34 ,
+                'avantage' => 'Montagnard',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Pister, piéger ou chasser, ainsi que pour d’autres activités similaires de survie.',
+            ],
+            [
+                'id'=> 35 ,
+                'avantage' => 'Né en selle',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Chevaucher des montures et agir en selle.',
+            ],
+            [
+                'id'=> 36 ,
+                'avantage' => 'Odorat développé',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Jet d’action sous esprit pour percevoir quelque chose grâce à votre odorat.',
+            ],
+            [
+                'id'=> 37 ,
+                'avantage' => 'Ouïe fine',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Jet d’action sous esprit pour percevoir quelque chose grâce à votre ouïe.',
+            ],
+            [
+                'id'=> 38 ,
+                'avantage' => 'Outillage',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Entreprendre une action avec vos outils ( en rapport avec votre métier ).',
+            ],
+            [
+                'id'=> 39 ,
+                'avantage' => 'Peau dure',
+                'attribut' => 'protection',
+                'attribut_bonus' => "1"
+
+            ],
+            [
+                'id'=> 40 ,
+                'avantage' => 'Perspicace',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Remarquer une entourloupe ou un mensonge.',
+            ],
+            [
+                'id'=> 40 ,
+                'avantage' => 'Pied marin',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Manœuvrer une embarcation ou entreprendre des activités physiques à bord d’un navire.',
+            ],
+            [
+                'id'=> 41 ,
+                'avantage' => 'Pisteur des marais',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Pister, piéger ou chasser, ainsi que pour d’autres activités similaires de survie.',
+            ],
+            [
+                'id'=> 42 ,
+                'avantage' => 'Poings d’acier',
+                'description' => 'Ajoutez votre vigueur aux dégâts quand vous combattez à mains nues.'
+            ],
+            [
+                'id'=> 43 ,
+                'avantage' => 'Pouvoir du Néant',
+                'attribut' => 'pouvoir',
+                'attribut_bonus' => "2",
+                'description' => 'Vous devez aussi prendre un désavantage supplémentaire.'
+            ],
+            [
+                'id'=> 44 ,
+                'avantage' => 'Récupération rapide',
+                'description' => 'Après un combat, vous regagnez 1 point de vitalité supplémentaire, En cas de blessure recupération de 1 point de vitalité par jour.'
+            ],
+            [
+                'id'=> 45 ,
+                'avantage' => 'Renard du désert',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Pister, piéger ou chasser, ainsi que pour d’autres activités similaires de survie.',
+            ],
+            [
+                'id'=> 46 ,
+                'avantage' => 'Résistant à la magie',
+                'description' => 'Lancez un d6. Sur un 6, le sortilège n’a aucun effet sur vous.'
+            ],
+            [
+                'id'=> 47 ,
+                'avantage' => 'Résistant aux poisons',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Résister aux effets des drogues, des venins, des toxines, ainsi que de l’alcool.',
+            ],
+            [
+                'id'=> 48 ,
+                'avantage' => 'Roi de la jungle',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Pister, piéger ou chasser, ainsi que pour d’autres activités similaires de survie.',
+            ],
+            [
+                'id'=> 49 ,
+                'avantage' => 'Roi de l’évasion',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Vous évader ou de vous libérer de vos liens.',
+            ],
+            [
+                'id'=> 50 ,
+                'avantage' => 'Santé de fer',
+                'description' => 'Vous êtes immunisé à toutes les maladies, y compris celles d’origine magique.'
+            ],
+            [
+                'id'=> 51 ,
+                'avantage' => 'Savant',
+                'attribut' => 'esprit',
+                'attribut_bonus' => "1"
+            ],
+            [
+                'id'=> 52 ,
+                'avantage' => 'Sentir la magie',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Reconnaître (ou de traquer) un sorcier, un effet ou un artefact magique.',
+            ],
+            [
+                'id'=> 53 ,
+                'avantage' => 'Tigre des neiges',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Pister, piéger ou chasser, ainsi que pour d’autres activités similaires de survie.',
+            ],
+            [
+                'id'=> 54 ,
+                'avantage' => 'Tireur puissant',
+                'description' => 'vous ajoutez votre vigueur aux dégâts (arc, javelot, fronde)'
+            ],
+            [
+                'id'=> 55 ,
+                'avantage' => 'Vigilant',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Jets de réaction.',
+            ],
+            [
+                'id'=> 56 ,
+                'avantage' => 'Vigilant',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Jets de réaction.',
+            ],
+            [
+                'id'=> 57 ,
+                'avantage' => 'Vigueur céruléenne',
+                'attribut' => 'vigueur',
+                'attribut_bonus' => "1"
+            ],
+            [
+                'id'=> 58 ,
+                'avantage' => 'Vigilant',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Jets de réaction.',
+            ],
+            [
+                'id'=> 59 ,
+                'avantage' => 'Vision nocturne',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Quand l’obscurité impose un malus à la vision.',
+            ],
+            [
+                'id'=> 60 ,
+                'avantage' => 'Vue perçante',
+                'de_bonus' => true,
+                'de_bonus_domaine' => 'Jet d’action sous esprit pour percevoir quelque chose grâce à votre vue',
             ],
             // Ajoutez d'autres régions selon vos besoins
             /*
