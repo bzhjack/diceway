32748828abf7d892e66cf5268eeb9c13ed1b83e3
bzhjack <bogomit@gmail.com>
2024-04-07 16:17:43 +0200

    plop

--- database/migrations/2024_04_02_144411_create_bol_regions_table.php
+++ database/migrations/2024_04_02_144411_create_bol_regions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('bol_regions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable()->default(null);
             $table->timestamps();
         });
     }
