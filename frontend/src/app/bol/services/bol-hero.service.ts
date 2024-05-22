bc54ad05bcd52d4cda337569f4168d18baadde85
bzhjack <bogomit@gmail.com>
2024-05-22 22:03:25 +0200

    plop

--- frontend/src/app/bol/services/bol-hero.service.ts
+++ frontend/src/app/bol/services/bol-hero.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {Observable} from "rxjs";
 import {BolHeroModel} from "../models/bol-hero.model";
 import {BolRegionModel} from "../models/bol-region.model";
