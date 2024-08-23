a11b9e197ec4cca99fdcaa90e6066e6b32a8b4b4
ljacques <lionel.jacques@equasens.com>
2024-08-23 11:38:46 +0200

    hide created updated

---
+++ database/migrations/2024_08_23_092419_create_table_bol_creature_capacite.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bol_creature_capacite', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('creature_id');
+            $table->unsignedBigInteger('capacite_id');
+            $table->timestamps();
+
+            // Déclaration des clés étrangères
+            $table->foreign('creature_id')->references('id')->on('bol_creature')->onDelete('cascade');
+
+            // On s'assure qu'une combinaison de region_id et avantage_id soit unique
+            $table->unique(['creature_id', 'capacite_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bol_creature_capacite');
+    }
+};
---
+++ database/migrations/2024_08_23_092508_create_table_bol_capacite.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bol_capacite', function (Blueprint $table) {
+            $table->id();
+            $table->string('capacite')->unique();
+            $table->boolean('de_bonus')->default(false);
+            $table->boolean('de_malus')->default(false);
+            $table->text('description')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bol_capacite');
+    }
+};
