42e8b0efa6d927dfe0457013bc739c1c4e396cb2
ljacques <lionel.jacques@equasens.com>
2024-09-18 14:00:38 +0200

    fix Number Type

--- frontend/src/app/bol/heros/create/ressources/ressources.component.ts
+++ frontend/src/app/bol/heros/create/ressources/ressources.component.ts
@@ -4,22 +4,27 @@ import {PrimeTemplate} from "primeng/api";
 import {ControlValueAccessor, FormBuilder, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule} from "@angular/forms";
 import {InputTextModule} from "primeng/inputtext";
 import {BolHerosStateService} from "../../../services/bol-heros-state.service";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {BolMessageComponent} from "../../../message/message.component";
 import {OverlayPanelModule} from "primeng/overlaypanel";
+import {InlineSVGModule} from "ng-inline-svg-2";
+import {MessagesModule} from "primeng/messages";
 
 @Component({
   selector: 'bol-heros-ressources',
   standalone: true,
-  imports: [
-    FieldsetModule,
-    PrimeTemplate,
-    ReactiveFormsModule,
-    InputTextModule,
-    NgIf,
-    BolMessageComponent,
-    OverlayPanelModule
-  ],
+    imports: [
+        FieldsetModule,
+        PrimeTemplate,
+        ReactiveFormsModule,
+        InputTextModule,
+        NgIf,
+        BolMessageComponent,
+        OverlayPanelModule,
+        InlineSVGModule,
+        MessagesModule,
+        NgForOf
+    ],
   templateUrl: './ressources.component.html',
   styleUrl: './ressources.component.scss',
   providers: [
