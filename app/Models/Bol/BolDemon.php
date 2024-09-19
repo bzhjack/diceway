a38314bd855873af127f5e2e652cc041c454747b
ljacques <lionel.jacques@equasens.com>
2024-09-19 17:09:12 +0200

    fix Number Type

---
+++ app/Models/Bol/BolDemon.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models\Bol;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class BolDemon extends Model
+{
+    use HasFactory, Uuids;
+
+    protected $table = 'bol_creature';
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+    protected $hidden = ['created_at', 'updated_at'];
+    protected $appends = ['type'];
+    protected $fillable = [
+        'id',
+        'user_id',
+        'nom',
+        'avatar',
+        'commentaire',
+
+        'vigueur',
+        'agilite',
+        'esprit',
+        'aura',
+
+        'melee',
+        'tir',
+        'defense',
+
+        'vitalite',
+        'degat',
+
+        'id_taille'
+    ];
+    protected $casts = [
+        'vigueur' => 'integer',
+        'agilite' => 'integer',
+        'esprit' => 'integer',
+        'aura' => 'integer',
+        'melee' => 'integer',
+        'tir' => 'integer',
+        'defense' => 'integer',
+        'vitalite' => 'integer',
+        'id_categorie' => 'integer'
+    ];
+
+    /*public function pouvoirs(): HasMany
+    {
+        return $this->HasMany(BolPouvoir::class, 'creature_id', 'id');
+    }*/
+    public function categorie(): HasOne
+    {
+        return $this->HasOne(BolCategorie::class, 'id', 'id_categorie');
+    }
+    public function getTypeAttribute() {
+         return $this->categorie->type;
+    }
+}
