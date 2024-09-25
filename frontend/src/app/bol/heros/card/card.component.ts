8d8e475519faa1e135c707948d88ef7ee189e46c
ljacques <lionel.jacques@equasens.com>
2024-09-25 17:52:44 +0200

    fix Number Type

---
+++ frontend/src/app/bol/heros/card/card.component.ts
@@ -0,0 +1,42 @@
+import {Component, computed, effect, inject, input, Signal, signal} from '@angular/core';
+import {BolHerosModel} from "../../models/bol-heros.model";
+import {BolHerosService} from "../../services/bol-heros.service";
+import {toObservable, toSignal} from "@angular/core/rxjs-interop";
+import {exhaustMap, filter} from "rxjs";
+import {tap} from "rxjs/operators";
+import {AsyncPipe, JsonPipe, NgIf} from "@angular/common";
+import {CardModule} from "primeng/card";
+import {SkeletonModule} from "primeng/skeleton";
+import {TooltipModule} from "primeng/tooltip";
+
+@Component({
+  selector: 'bol-heros-card',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    JsonPipe,
+    CardModule,
+    SkeletonModule,
+    NgIf,
+    TooltipModule
+  ],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class BolHerosCardComponent {
+  heroService = inject(BolHerosService);
+  heroId = input<string | null>(null);
+  hero = signal<BolHerosModel | null>(null);
+
+  hero$ = toObservable<string | null>(this.heroId).pipe( // Watch for user changes
+    filter((id) => id !== null),                     // Only make http request for users larger than 0
+    tap((id) => console.log('user id', id)),    // Just some debugging
+    exhaustMap((id) =>                          // Don't execute the http request if one is already in progress
+      this.heroService.heros(id as string).pipe()
+       // Make the http request
+        .pipe(tap((hero) => this.hero.set(hero)))   // Update the response
+    )
+  );
+  constructor() {
+  }
+}
--- frontend/src/app/playground/playground.component.ts
+++ frontend/src/app/playground/playground.component.ts
@@ -1,72 +1,16 @@
-import {AfterViewInit, Component} from '@angular/core';
-import DiceBox from "@3d-dice/dice-box";
-import DisplayResults from "@3d-dice/dice-ui/src/displayResults";
-import AdvancedRoller from "@3d-dice/dice-ui/src/advancedRoller";
-import BoxControls from "@3d-dice/dice-ui/src/boxControls";
+import {Component} from '@angular/core';
+import {BolHerosCardComponent} from "../bol/heros/card/card.component";
 
 
 @Component({
   selector: 'app-playground',
   standalone: true,
-  imports: [],
+  imports: [
+    BolHerosCardComponent
+  ],
   templateUrl: './playground.component.html',
   styleUrl: './playground.component.scss'
 })
-export class PlaygroundComponent implements AfterViewInit{
-ngAfterViewInit() {
-  let Box = new DiceBox("#dici-box", {
-    assetPath: "/frontend/assets/dice/",
-    theme: "default",
-    offscreen: true,
-    scale: 6
-  });
-  Box.init().then(async (world: any) => {
-    // console.log("Box is ready");
+export class PlaygroundComponent {
 
-    const Controls = new BoxControls({
-      themes: ["default", "rust", "diceOfRolling", "gemstone"],
-      themeColor: world.config.themeColor,
-      onUpdate: (updates: any) => {
-        Box.updateConfig(updates);
-      }
-    });
-    Controls.themeSelect.setValue(world.config.theme);
-
-    Box.onThemeConfigLoaded = (themeData: any) => {
-      if (themeData.themeColor) {
-        Controls.themeColorPicker.setValue(themeData.themeColor);
-      }
-    };
-
-    // create display overlay
-    const Display = new DisplayResults("#dici-box");
-
-    // // create Roller Input
-    const Roller = new AdvancedRoller({
-      target: "#dici-box",
-      onSubmit: (notation: any) => {
-        console.log("notation", notation);
-        Box.roll(notation)
-      },
-      onClear: () => {
-        Box.clear();
-        Display.clear();
-      },
-      onReroll: (rolls: any) => {
-        // loop through parsed roll notations and send them to the Box
-        rolls.forEach((roll: any) => Box.add(roll, roll.groupId));
-      },
-      onResults: (results: any) => {
-        console.log("result", results);
-        Display.showResults(results);
-      }
-    });
-
-    // pass dice rolls to Advanced Roller to handle
-    Box.onRollComplete = (results: any) => {
-      Roller.handleResults(results);
-    };
-   Box.roll(["4d20", "4d12", "4d10", "4d8", "4d6", "4d4"]);
-  });
-}
 }
