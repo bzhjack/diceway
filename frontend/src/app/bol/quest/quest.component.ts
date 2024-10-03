ae1cf0ac48d97a71832d09f444154ed0e613e6b7
ljacques <lionel.jacques@equasens.com>
2024-10-03 14:02:11 +0200

    fix Number Type

---
+++ frontend/src/app/bol/quest/quest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bol-quest',
+  standalone: true,
+  imports: [],
+  templateUrl: './quest.component.html',
+  styleUrl: './quest.component.scss'
+})
+export class BolQuestComponent {
+
+}
