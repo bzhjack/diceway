b9c4072ac5eeb90ae3461b999abcccab3985f930
ljacques <lionel.jacques@equasens.com>
2025-10-21 14:40:16 +0200

    rename front

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,25 +1,12 @@
-import {ApplicationConfig} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {routes} from './app.routes';
-import {JwtInterceptor} from './auth/interceptors/jwt-interceptor';
-import {UnauthorizedInterceptor} from './auth/interceptors/unauthorized-interceptor';
-import {UserService} from './auth/services/user.service';
-import {MessageService} from "primeng/api";
-import {DialogService} from "primeng/dynamicdialog";
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(
-      withInterceptors([JwtInterceptor, UnauthorizedInterceptor]),
-    ),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-    UserService,
-    MessageService,
-    DialogService
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes)
   ]
 };
-
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  // Keep empty to use relative paths and dev proxy
-  apiBase: ''
-};
