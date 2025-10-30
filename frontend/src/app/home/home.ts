86672ddede9ce20578a0fce6680548616891af65
ljacques <lionel.jacques@equasens.com>
2025-10-30 15:11:37 +0100

    remove svg

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {SmokeScene} from '../smoke/smoke-scene';
 
 @Component({
   selector: 'app-home',
   imports: [
-    SmokeScene
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
