39fa523352bdceb7a100c8288508d398f7cbcde6
ljacques <lionel.jacques@equasens.com>
2025-11-05 11:23:26 +0100

    add playground

---
+++ frontend/src/app/bol/bol-playground/bol-playground.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BolPlayground } from './bol-playground';
+
+describe('BolPlayground', () => {
+  let component: BolPlayground;
+  let fixture: ComponentFixture<BolPlayground>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BolPlayground]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BolPlayground);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/bol/bol-playground/bol-playground.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bol-playground',
+  imports: [],
+  templateUrl: './bol-playground.html',
+  styleUrl: './bol-playground.scss',
+})
+export class BolPlayground {
+
+}
