bcbb61af99d43d935a619b6a8ea87718516c4bd4
ljacques <lionel.jacques@equasens.com>
2025-11-21 16:30:49 +0100

    add playground

---
+++ frontend/src/app/bol/bol-heros/bol-hero-card/bol-hero-card.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-bol-hero-card',
+  imports: [],
+  templateUrl: './bol-hero-card.html',
+  styleUrl: './bol-hero-card.scss',
+})
+export class BolHeroCard {
+
+}
